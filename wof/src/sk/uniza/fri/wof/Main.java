05eb3da92978139823e2298c896b231d0e95a0cf
Michal Ďuračík <Michal.Duracik@fri.uniza.sk>
2022-04-29 10:26:11 +0200

    Nacitanie yml pomocou snakeYML

--- wof/src/sk/uniza/fri/wof/Main.java
+++ wof/src/sk/uniza/fri/wof/Main.java
@@ -1,11 +1,14 @@
 package sk.uniza.fri.wof;
 
+import org.yaml.snakeyaml.Yaml;
 import sk.uniza.fri.wof.hra.Hra;
 import sk.uniza.fri.wof.nacitavacMapy.NacitavacSuboru;
 import sk.uniza.fri.wof.nacitavacMapy.Riadok;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Hlavna trieda hry WoF s metodou main - spustanie v NB
@@ -29,6 +32,10 @@ public class Main {
      * @param args parametre programu
      */
     public static void main(String[] args) throws IOException {
+        Yaml yaml = new Yaml();
+        Map<String, Object> obj = yaml.load(new FileInputStream("mapa.yml"));
+        System.out.println(obj);
+
 
         Hra hra = new Hra();
         hra.hraj();
